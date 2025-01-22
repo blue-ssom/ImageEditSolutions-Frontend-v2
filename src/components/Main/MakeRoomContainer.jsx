b536bd9e89351e83dc7d5df5e90e9568142e88c7
Somi Yoo <79749558+blue-ssom@users.noreply.github.com>
2025-01-22 21:00:17 +0900

    feat: handleCreateRoom 함수에서 nav를 모달로 전달하도록 로직 분리

--- src/components/Main/MakeRoomContainer.jsx
+++ src/components/Main/MakeRoomContainer.jsx
@@ -8,12 +8,20 @@ const MakeRoomContainer = () => {
 
   const [isOpenJoinRoom, setIsOpenJoinRoom] = useState(false);
   const [roomNumber, setRoomNumber] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false); // [ 추가/작성자:YSM ] 모달 열림 여부
+  const [modalMessage, setModalMessage] = useState(""); // [ 추가/작성자:YSM ] 모달에 표시될 메시지
 
   const handleCreateRoom = async () => {
     setIsOpenJoinRoom(false);
 
     const authToken = localStorage.getItem('accessToken');
-    if(!authToken) nav('/login');
+
+    // [ 수정/작성자:YSM ]
+    if(!authToken) {
+      setModalMessage("로그인이 필요합니다.");
+      setIsModalOpen(true);
+      return;
+    }
 
     const newRoomId = await createRoom();
 
@@ -47,10 +55,36 @@ const MakeRoomContainer = () => {
       setIsOpenJoinRoom(true);
     }
   }
+
+  // [ 추가/작성자:YSM ] 모달 닫기 함수
+  const closeModal = () => {
+    setIsModalOpen(false);
+    nav('/login');
+  };
+
+  // [ 추가/작성자:YSM ] 모달 컴포넌트
+  const Modal = ({ message, onClose }) => {
+    return (
+      <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-800 bg-opacity-50 z-50">
+        <div className="bg-[#1F2937] rounded-lg shadow-lg w-[480px] h-[240px] p-6 flex flex-col justify-between">
+          <h2 className="text-2xl text-blue-200 mb-4 text-center">알림</h2>
+          <p className="text-white text-lg text-center mb-6">{message}</p>
+          <button
+            onClick={onClose}
+            className="w-1/3 mx-auto py-2 px-4 text-white border border-white rounded hover:bg-gray-700 focus:outline-none transition duration-300"
+          >
+          확인
+          </button>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <div className="w-100 mx-auto mt-20">
       <div className="flex flex-col items-center justify-center">
-        <label className="text-[20px] text-[#B8B8B8] mb-5">이미지를 함께 작업해보세요</label>
+        {/* [ 수정/작성자:YSM ] label 글자 스타일 수정 */}
+        <label className="text-xl font-bold text-white mb-4">이미지를 함께 작업해보세요</label>
         <div
           className={`w-80 h-14 rounded-lg flex items-center justify-between pr-2 box-border bg-gray-800 overflow-hidden`}
         >
@@ -94,6 +128,8 @@ const MakeRoomContainer = () => {
           <img src="/images/home/make-room.svg" alt="make-room" className="w-6 mr-1" />
           방 입장하기
         </button>
+         {/* [ 추가/작성자:YSM ] 모달 렌더링 */}
+         {isModalOpen && <Modal message={modalMessage} onClose={closeModal} />}
       </div>
     </div>
   )
